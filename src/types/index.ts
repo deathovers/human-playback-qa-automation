8f7aa4ef66f42ac6bd3c52f017a4f8444bc860b5
deathovers <88528959+deathovers@users.noreply.github.com>
2026-03-19 22:07:08 +0530

    Add types

---
+++ src/types/index.ts
@@ -0,0 +1,30 @@
+export interface BrowserConfig {
+  headless: boolean;
+  slowMo?: number;
+}
+
+export interface TestInput {
+  testId: string;
+  url: string;
+  timeoutSeconds: number;
+  browserConfig: BrowserConfig;
+}
+
+export interface TestMetrics {
+  loadTimeMs: number;
+  playbackStartTimeMs: number;
+  durationValidatedSec: number;
+}
+
+export interface TestError {
+  message: string | null;
+  screenshotPath: string | null;
+}
+
+export interface TestOutput {
+  testId: string;
+  status: 'SUCCESS' | 'FAIL';
+  metrics: TestMetrics;
+  error: TestError;
+  timestamp: string;
+}
